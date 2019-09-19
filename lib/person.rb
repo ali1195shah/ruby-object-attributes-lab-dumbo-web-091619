c99ec9c100566a993dc02c661af2c71638a7d7ab
Ali Shah <ali1195shah@gmail.com>
2019-09-19 13:01:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,4 +4,26 @@ class Person
     @name
   end
   
+  # -----------------------
+  # Getters
+  
+  def name
+    @name
+  end
+  
+  def job
+    @job
+  end
+  
+  -----------------------
+  # Setters
+  
+  def name=(new_name)
+    @name
+  end
+  
+  def job=(new_job)
+    @job
+  end
+  
 end
