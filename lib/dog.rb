e99f48c7bcb7048a12fa507bdaa0eead4cdd144f
Ali Shah <ali1195shah@gmail.com>
2019-09-19 12:57:35 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog 
+  
+  def instance(name, breed)
+    @name
+    @breed
+  end
+  
+  # --------------------
+  # Getters
+  
+  def name 
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  # --------------------
+  # Setters
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed=(new_breed)
+    @breed
+  end
+  
+  
+end
